d89d9b3fc92ccac3b75387537e6dd73ed084763d
Andrea Baldo <andrixb@gmail.com>
2022-11-22 16:53:14 +0100

    feat(pagination and list): Implement virtualization and list view
    Signed-off-by: Andrea Baldo <andrixb@gmail.com>

---
+++ src/itemsManagerCtx/ui/components/ListItemComponent.tsx
@@ -0,0 +1,23 @@
+import { Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, ListItem } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+interface ListItemComponentProps {
+    title?: string;
+    image?: string;
+    children?: JSX.Element;
+}
+
+export const ListItemComponent = ({ title, image, children }: ListItemComponentProps) => (
+    <ListItem component="div" style={{ padding: '1rem 0.5rem' }}>
+        <Card sx={{ maxWidth: '100%' }}>
+            <CardHeader title={title} />
+            <CardMedia component="img" height="194" image={image} alt={title} />
+            <CardContent>{children}</CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    </ListItem>
+);
--- src/itemsManagerCtx/ui/index.tsx
+++ src/itemsManagerCtx/ui/index.tsx
@@ -1,3 +1,28 @@
-export default function ItemsManagerPageHome() {
-    return (<></>);
+import { Box, Typography } from '@mui/material';
+
+import { Virtuoso } from 'react-virtuoso';
+
+import { useGetItems } from '../infrastructure/hooks';
+import { ListItemComponent } from './components';
+
+function ItemsManagerPageHome() {
+    const { currentItems } = useGetItems();
+
+    return (
+        <Box>
+            {!!currentItems && (
+                <Virtuoso
+                    useWindowScroll
+                    data={currentItems}
+                    itemContent={(index, item) => (
+                        <ListItemComponent key={index} title={item.title} image={item.image}>
+                            <Typography variant="body1">{item.description}</Typography>
+                        </ListItemComponent>
+                    )}
+                />
+            )}
+        </Box>
+    );
 }
+
+export default ItemsManagerPageHome;
