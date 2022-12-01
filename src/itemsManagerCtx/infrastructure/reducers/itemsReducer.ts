199d7f61af17594e53d70802d221ed3e3685e81a
Andrea Baldo <andrixb@gmail.com>
2022-12-01 15:42:30 +0100

    feat(sort): add sort

--- src/itemsManagerCtx/infrastructure/reducers/itemsReducer.ts
+++ src/itemsManagerCtx/infrastructure/reducers/itemsReducer.ts
@@ -1,4 +1,4 @@
-import { GET_ITEMS, ItemsActionTypes } from '../actions';
+import { GET_ITEMS, ItemsActionTypes, SORT_BY_DESCRIPTION, SORT_BY_EMAIL, SORT_BY_PRICE, SORT_BY_TITLE } from '../actions';
 import { ItemsState } from '../interfaces';
 
 export const itemsInitialState: ItemsState = {
@@ -9,6 +9,26 @@ export const itemsReducer = (state: ItemsState, action: ItemsActionTypes) => {
     const { type, payload } = action;
     switch (type) {
         case GET_ITEMS:
+            return {
+                ...state,
+                items: payload.items,
+            };
+        case SORT_BY_TITLE:
+            return {
+                ...state,
+                items: payload.items,
+            };
+        case SORT_BY_EMAIL:
+            return {
+                ...state,
+                items: payload.items,
+            };
+        case SORT_BY_PRICE:
+            return {
+                ...state,
+                items: payload.items,
+            };
+        case SORT_BY_DESCRIPTION:
             return {
                 ...state,
                 items: payload.items,
