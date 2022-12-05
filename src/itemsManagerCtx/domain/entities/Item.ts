de93dcf9bd8383be4d004cea1aaaec2e89423c41
Andrix <andrixb@gmail.com>
2022-12-05 11:59:15 +0100

    chore: fix uuid library
    Signed-off-by: Andrix <andrixb@gmail.com>

--- src/itemsManagerCtx/domain/entities/Item.ts
+++ src/itemsManagerCtx/domain/entities/Item.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 import { Properties } from '../../../shared/infrastructure/types/FunctionPropertiesType';
 
 export interface IItem {
