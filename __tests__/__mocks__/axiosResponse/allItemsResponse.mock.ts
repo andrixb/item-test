2ce8a6b2007165a6007fb0110c710a557e8d13b0
Andrix <andrixb@gmail.com>
2022-12-08 16:38:34 +0100

    chore: Work on tests
    Signed-off-by: Andrix <andrixb@gmail.com>

---
+++ __tests__/__mocks__/axiosResponse/allItemsResponse.mock.ts
@@ -0,0 +1,6 @@
+
+import { allItemsMock } from '../items/allItems.mock';
+import { axiosOKBaseResponseMock } from './axiosBaseResponse.mock';
+
+export const itemsAllAxiosResponseMock = () =>
+    axiosOKBaseResponseMock(allItemsMock);
--- src/itemsManagerCtx/infrastructure/repositories/getItems.spec.ts
+++ src/itemsManagerCtx/infrastructure/repositories/getItems.spec.ts
@@ -1,41 +1,20 @@
 /// <reference types="jest" />
 import { jest, expect } from '@jest/globals';
-import axios from 'axios';
-import { allItemsMock } from '../../../../__tests__/__mocks__/items/allItems.mock';
-import { adaptItemsResponse } from '../adapters/adaptItemsResponse';
+import { itemsAllAxiosResponseMock } from '../../../../__tests__/__mocks__/axiosResponse/allItemsResponse.mock';
+import { apiInstance } from '../../../shared/infrastructure/api/apiClient';
 import { getItems, GetItemsRequest } from './getItems';
-jest.mock('axios');
-const axiosMock = axios as jest.Mocked<typeof axios>;
+
+
+const getStub = jest.spyOn(apiInstance, 'get');
 
 describe('Get Items Repository', () => {
     const requestItemsData: GetItemsRequest = {
         searchParams: {},
     };
 
-    beforeEach(() => {
-        axiosMock.get.mockClear();
-    });
-
     it('should return a list of items', async () => {
-        axiosMock.get.mockResolvedValue({
-            data: allItemsMock,
-        });
-
-        const results = await getItems(requestItemsData);
-        const adaptedItemsMockResults = adaptItemsResponse(allItemsMock);
-        expect(results).toEqual(adaptedItemsMockResults);
-    });
-
-    it('should not return a list of items', async () => {
-        const errorResponseData = {
-            data: undefined,
-        };
-        axiosMock.get.mockRejectedValue('Error');
-        axiosMock.get.mockResolvedValue({
-            data: [],
-        });
-
-        const results = await getItems(requestItemsData);
-        expect(results).toEqual(errorResponseData);
+        getStub.mockImplementationOnce(async () => itemsAllAxiosResponseMock());
+        const { items } = await getItems(requestItemsData);
+        expect(items).toBeDefined();
     });
 });
